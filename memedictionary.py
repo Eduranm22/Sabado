f0afd79234dae44bde68147a98fd0e41269a1f53
Eduranm22 <eduranrd22@gmail.com>
2024-08-24 18:41:33 -0400

    memedictionary.py

--- memedictionary.py
+++ memedictionary.py
@@ -2,7 +2,10 @@ meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
             "LMAO": "Otra respuesta común a algo gracioso",
-            "RIZZ": "La capacidad de atraer a una pareja romántica o sexual."
+            "RIZZ": "La capacidad de atraer a una pareja romántica o sexual",
+            "CREEPY": "Aterrador, siniestro",
+            "SHEESH": "Ligera desaprobacion",
+            "AGGRO": "ponerse agresivo/enojado"
             }
  
 for i in range(5):           
