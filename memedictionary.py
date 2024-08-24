d26e9544b93434bd3fe2ce6dc04ea32e96f38da3
Eduranm22 <eduranrd22@gmail.com>
2024-08-24 18:39:12 -0400

    memedictionary.py

--- memedictionary.py
+++ memedictionary.py
@@ -4,10 +4,12 @@ meme_dict = {
             "LMAO": "Otra respuesta común a algo gracioso",
             "RIZZ": "La capacidad de atraer a una pareja romántica o sexual."
             }
-            
-word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
-
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print('Esta palabra no esta en el diccionario!')
+ 
+for i in range(5):           
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+    
+    
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print('Esta palabra no esta en el diccionario!')
